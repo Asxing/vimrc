4cab1ed1e78311eb26d2333a697796ca98cb3bee
holddie <holddie@163.com>
2020-06-10 17:45:39 +0800

    deploy

--- moveToRepo.sh
+++ moveToRepo.sh
@@ -12,3 +12,9 @@ cp -f  ~/.ideavimrc idea
 echo "copy $ideavimrcFilePath to vimrc"
 cp -r -f ~/.SpaceVim.d  spacevim
 echo 'copy spacevim.d to spacevim '
+
+git add -A
+
+git commit -m 'deploy'
+
+git push
