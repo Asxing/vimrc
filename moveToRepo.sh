b9ba0206c69b470df5051af0747d43292d4d08d4
holddie <holddie@163.com>
2020-06-10 17:38:18 +0800

    add moveToRepo.sh

---
+++ moveToRepo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+vimrc1FilePath="~/.vimrc\~"
+
+ideavimrcFilePath="~/.ideavimrc"
+
+
+cp -f  ~/.vimrc~ vimrc
+mv vimrc/.vimrc~ vimrc/.vimrc
+echo "copy $vimrc1FilePath to vimrc"
+cp -f  ~/.ideavimrc idea 
+echo "copy $ideavimrcFilePath to vimrc"
+cp -r -f ~/.SpaceVim.d  spacevim
+echo 'copy spacevim.d to spacevim '
